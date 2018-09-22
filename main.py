77f4fca920f8d17debfb089c1596d6c45e981b53
jaganathsai <42360458+jaganathsai@users.noreply.github.com>
2018-09-21 22:14:58 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
 import os
 import csv
 
-ElecDataCSV = os.path.join(".", "Resources", "election_data.csv")
+PyBankCSV = os.path.join(".", "Resources", "budget_data.csv")
 
-TotalnumofVotes=0
-CanditatesVotes={}
-a = 0
+TotalnumofMonths=1
+TotalNetAmount = 0
+TotalChange = 0
+profit=0
+loss=0
 
 
 # with open(udemy_csv, newline="", encoding='utf-8') as csvfile:
-with open(ElecDataCSV, newline="") as ElecDataCSVfile:
-    CsvReader = csv.reader(ElecDataCSVfile, delimiter=",")
+with open(PyBankCSV, newline="") as PyBankCSVfile:
+    CsvReader = csv.reader(PyBankCSVfile, delimiter=",")
     CsvHeader = next(CsvReader)
+    CsvRowPrev = next(CsvReader)
    
+    TotalNetAmount = int(CsvRowPrev[1])
     
     for row in CsvReader:
-        TotalnumofVotes = TotalnumofVotes + 1
-        if row[2] not in CanditatesVotes:
-            CanditatesVotes[row[2]] = a
-        if row[2]  in CanditatesVotes:
-               CanditatesVotes[row[2]] = CanditatesVotes.get(row[2]) +1
 
-    
-    print("Election Results")
-    print("---------------------")
-    print(f"Total Votes : {TotalnumofVotes}")
-    print("---------------------")
-    
-    for key in  CanditatesVotes:
-        print(f" {key}:{(CanditatesVotes[key]/TotalnumofVotes) * 100}% ({CanditatesVotes[key]})")
+        # Total number of months
+
+        TotalnumofMonths = TotalnumofMonths+1
+
+        TotalNetAmount = int(row[1]) + TotalNetAmount
+
+        MonthlyChange = int(row[1]) - int(CsvRowPrev[1])
+
+        TotalChange = MonthlyChange + TotalChange
+
+        if MonthlyChange > 0:
+            if MonthlyChange > profit:
+                profit=MonthlyChange
+                profitMonth=row[0]
+        elif MonthlyChange < 0:
+            if abs(MonthlyChange) > abs(loss):
+                loss=MonthlyChange
+                lossMonth=row[0]
+        
+        CsvRowPrev=row
+
+        AverageChange=TotalChange/(TotalnumofMonths-1)
 
 
-
-
-
-
+    print("Financial Analysis")
+    print("-------------------")
+    print(f"Total Months: {TotalnumofMonths}")
+    print(f"Total : {TotalNetAmount}")
+    print(f"Average Change: {AverageChange}")
+    print(f"Greatest Increase in Profits: {profitMonth} ({profit})")
+    print(f"Greatest Increase in Profits: {lossMonth} ({loss})")
 
 
 
